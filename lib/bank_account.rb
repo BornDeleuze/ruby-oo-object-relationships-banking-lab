342c0b6539bd0fea6cadd1197253b983895bcd3c
Matt Beaudoin <ehxtwo@gmail.com>
2021-03-09 13:09:57 -0800

    commit tree 96afb699ce9719e534496c4b1319885c94c1bd29

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,33 @@
+require 'pry'
+
 class BankAccount
+    attr_accessor :status, :balance
+    attr_reader :name
+
+    def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+    end
+
+    def deposit(i)
+        @balance += i
+    end
+
+    def display_balance 
+        "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+        status = true
+        if self.balance < 1 || self.status == "closed"
+            status = false
+        end
+        status
+    end
+
+    def close_account
+        self.status = "closed"
+    end
 
 end
